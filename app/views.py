6e428bf55aa05a2785d62f41d9949faa2a22954e
treystaff <treystaff@gmail.com>
2015-12-13 11:01:19 -0600

    Tutorial part 2.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,28 @@
+from flask import render_template
 from app import app
 
 # The first view.
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello_world!"
+    # For demonstrative purposes, create a view that welcomes a
+    #   logged in user.
+
+    # Create an example user
+    user = {'nickname': 'Trey'}
+
+    # Create some fake posts
+    posts = [
+        {
+            'author': {'nickname': 'Will'},
+            'body': 'Portland is rainy'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The movie was so cool!'
+        }
+    ]
+
+    # Render the index.html template in the app/static/ directory
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
